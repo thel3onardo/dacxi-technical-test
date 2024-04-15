dc6760a10e7063d6257297645ec48c65d531a2ee
thel3onardo <leonardo.contato03@gmail.com>
2024-04-15 16:06:35 -0300

    feat(client): button and card components

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const variants = {
+  primary: 'bg-gradient-to-b from-[#8E50DE] to-[#421183] ring-[#9E66E7] text-[#D9D9D9]',
+  default: 'bg-gradient-to-b from-[#171718] to-[#0F0F12] ring-[#292929]'
+}
+
+const roundOptions = {
+  sm: 'rounded-sm',
+  md: 'rounded-md',
+  lg: 'rounded-lg',
+  full: 'rounded-full'
+}
+
+type Variant = keyof typeof variants
+type RoundOption = keyof typeof roundOptions
+
+type Props = {
+  variant: Variant
+  rounded: RoundOption
+  icon?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  variant: 'default',
+  rounded: 'lg'
+})
+</script>
+
+<template>
+  <!-- TODO: iprove this simple logic -->
+  <button
+    :class="`py-2 px-5 rounded-lg font-medium text-light ${variants[variant]} ${roundOptions[rounded]}`"
+  >
+    <slot />
+  </button>
+</template>
---
+++ src/components/ui/Card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="border border-[#D9D9D9]/5 rounded-2xl backdrop-blur-lg p-12">
+    <slot />
+  </div>
+</template>
