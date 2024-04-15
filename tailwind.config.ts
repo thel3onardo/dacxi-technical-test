92dac58c6f196e9c3999eb56bbe84441372cf7b0
thel3onardo <leonardo.contato03@gmail.com>
2024-04-15 15:09:59 -0300

    chore: add basic design system colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+import { Config } from 'tailwindcss'
+
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   content: [],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#AA69FF',
+        light: '#e0e0e0',
+        surface: '#000005'
+      }
+    }
   },
   plugins: []
-}
+} satisfies Config
